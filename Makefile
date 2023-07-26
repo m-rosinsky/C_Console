3830080b9bfd39cac2a830e35b8e03ff5a4b739c
m-rosinsky <michael.rosinsky@uconn.edu>
2023-07-26 22:15:48 +0000

    Removed queue impl

--- Makefile
+++ Makefile
@@ -24,14 +24,12 @@ setup:
 compile: setup
 	@echo -n "Compiling sources..."
 
-	@$(CC) $(CFLAGS) -o $(OBJS)/queue.o -c source/common/queue.c
-
 	@echo "   done"
 
 link: setup compile
 	@echo -n "Linking binaries..."
 
-	@$(CC) $(CFLAGS) -o $(BINS)/$(EXEC) source/main.c $(OBJS)/*.o
+	@$(CC) $(CFLAGS) -o $(BINS)/$(EXEC) source/main.c # $(OBJS)/*.o
 
 	@echo "   done"
 
--- source/main.c
+++ source/main.c
@@ -9,53 +9,9 @@
 
 #include <stdio.h>                              // printf()
 
-#include "common/queue.h"
-
 int
 main ()
 {
-    queue_t * p_q = queue_create();
-    if (NULL == p_q)
-    {
-        printf("create\n");
-        return 1;
-    }
-
-    int nums[5] = {1,2,3,4,5};
-    for (size_t i = 0; i < 5; ++i)
-    {
-        if (-1 == queue_enq(p_q, nums + i))
-        {
-            printf("enq\n");
-            return 1;
-        }
-    }
-
-    int * p_i = NULL;
-    printf("[");
-    while (0 != p_q->size)
-    {
-        p_i = queue_deq(p_q);
-        if (NULL == p_i)
-        {
-            printf("deq\n");
-            return 1;
-        }
-        printf("%d", *p_i);
-        if (0 != p_q->size)
-        {
-            printf(", ");
-        }
-    }
-    printf("]\n");
-
-    if (-1 == queue_destroy(p_q))
-    {
-        printf("destroy\n");
-        return 1;
-    }
-    p_q = NULL;
-
     printf("success\n");
     return 0;
 }
